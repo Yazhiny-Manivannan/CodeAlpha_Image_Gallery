ae6a1bd7ef48a2814b6befff876164d08911dde7
Yazhiny Manivannan <yazhinym@gmail.com>
2025-05-29 20:42:49 +0530

    deleted

--- script.js
+++
@@ -1,8 +0,0 @@
-function openImage(element) {
-  document.getElementById('fullscreen').style.display = 'flex';
-  document.getElementById('fullscreen-img').src = element.src;
-}
-
-function closeImage() {
-  document.getElementById('fullscreen').style.display = 'none';
-}
